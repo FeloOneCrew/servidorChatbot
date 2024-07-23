4c6aa452cf913243bf59f32b2874c5b46d4ee1fb
JaimeMenoyos <softjaimito27@gmail.com>
2024-07-23 17:26:20 -0500

    base de datos y rest

--- routes/rutasAPI.py
+++ routes/rutasAPI.py
@@ -7,14 +7,15 @@ from database.basedatos import conexion
 rutas = APIRouter()
 
 @rutas.get("/asegurachat")
-
 def consultarChat():
     try:
         consulta =  select(chat)
+        print("hola")
         #Entregueme todo lo de la consulta
-        resultado = conexion.execute(consulta).fetchall
+        resultado = conexion.execute(consulta).fetchall()
         resultadoJSON = [{'id': row.id, 'pregunta': row.pregunta, 'respuesta':row.respuesta} for row in resultado]
         return resultadoJSON
 
     except Exception as error:
+        print(error)
         raise HTTPException(status_code = 500, detail=str(error))
